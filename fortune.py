f6a57a40eb977a46cc8f841a22e5cc99765cae06
vasundhara debbarma <vasundharadebbarma60@gmail.com>
2026-09-25 02:58:41 +0530

    first version

---
+++ fortune.py
@@ -0,0 +1,11 @@
+import random
+
+fortune=[
+    "somthing amazing is coming your way",
+    "you will meet someone important today",
+    "you are super close than you think ",
+    " bye bye your luck is bad 😂 ",
+]
+
+print("your fortune")
+print(random.choice(fortune))
